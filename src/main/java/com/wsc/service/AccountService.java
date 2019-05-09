55a68792bd52f02ef6d7323378831c72c5811ca9
e <569188901@qq.com>
2019-05-09 16:16:40 +0800

    非检查异常回滚处理

--- src/main/java/com/wsc/service/AccountService.java
+++ src/main/java/com/wsc/service/AccountService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLException;
+
 @Service
 public class AccountService {
 
@@ -17,16 +19,16 @@ public class AccountService {
         return accountMapper.getAccount();
     }
 
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public void addMoney() throws Exception {
         //先增加余额
         accountMapper.addMoney();
 
         //然后遇到故障
-
         //遇到运行异常（RuntimeException）和程序错误（Error）可以回滚
-        throw new RuntimeException("发生异常了..");
+        //throw new RuntimeException("发生异常了..");
 
-
+        //遇到非检测异常,不回滚，除非加
+        throw new SQLException("发生异常了..");
     }
 }
